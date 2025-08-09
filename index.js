23d990db0d94430c680eed4e70bf5fac4084b162
Hoang Quoc Ly <hoanglyquoc@gmail.com>
2025-08-09 22:20:40 +0700

    simple random shake it button

--- index.js
+++ index.js
@@ -8,9 +8,22 @@ const port = 3000;
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// REST API
 app.get('/', (req, res) => {
     res.render("index.ejs");
+})
+
+// REST API
+app.get('/random', async (req, res) => {
+    try {
+        const response =  await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/random.php`)
+        const data = response.data.drinks[0];
+        res.render("index.ejs", { 
+            name: data.strDrink,
+            instructions: data.strInstructions
+        });
+    } catch (error) {
+        res.render("index.ejs", { content: JSON.stringify(error.response.data)});
+    }
 })
 
 app.listen(port, () => {
